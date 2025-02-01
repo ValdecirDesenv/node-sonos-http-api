9d66d3eef14599b367d8eb608d879922bbeea285
ValdecirDesenv <valdecir.neper@gmail.com>
2025-02-01 17:03:57 -0500

    fix updates

--- frontController/src/components/GroupDetails.jsx
+++ frontController/src/components/GroupDetails.jsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/styles.css";
 import React, { useState, useEffect } from "react";
-
+import TimeRangeSelector from "./TimeRangeSelector";
 import { Card, CardBody, CardTitle, CardText, Table } from "reactstrap"; // Bootstrap components
 import ToggleSwitch from "./ToggleSwitch";
 import { useWebSocketContext } from "../hooks/WebSocketProvider";
@@ -10,13 +10,18 @@ const GroupDetails = ({ group }) => {
   if (!group || !group.data || group.data.length === 0) {
     return <div>Loading...</div>;
   }
-  const [offlineData, setOfflineData] = useState("custom-bg");
+  if (group.type !== "initial") {
+    console.log("GroupDetails: Invalid group type", group);
+    return null;
+  }
+  const [offLineData, setOffLineData] = useState("custom-bg");
+  const [timeRange, setTimeRange] = useState({ start: "08:00", end: "22:00" });
 
   useEffect(() => {
-    if (group.offlineData) {
-      setOfflineData("custom-bg-offline");
+    if (group.offLineData) {
+      setOffLineData("custom-bg-offline");
     }
-  }, [group.offlineData]);
+  }, []);
 
   const [toggleStates, setToggleStates] = useState({});
 
@@ -38,20 +43,22 @@ const GroupDetails = ({ group }) => {
   };
 
   return (
-    <div className="container-fluid my-4 custom-bg">
+    <div className={`container-fluid my-4 custom-bg ${offLineData}`}>
       <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-        {group.data.map((groupItem) => {
+        {group.data.map((groupItem, index) => {
           const { coordinator, members } = groupItem;
           const { roomName, state } = coordinator;
           const { volume, mute, playbackState } = state;
-          const toggleKey = groupItem.id;
+          const toggleKey = groupItem.uuid;
           const isToggled = groupItem.keepPlaying;
-          const cardBodyClassZone = groupItem.offLineZone
-            ? "custom-bg-offline"
+          const cardBodyClassZone = group.offLineData
+            ? "custom-bg-offlineCard"
+            : groupItem.offLineZone
+            ? "custom-bg-offlineCard"
             : "custom-bg-2";
 
           return (
-            <div className="col custom-bg-4" key={groupItem.id}>
+            <div className="col custom-bg-4" key={index}>
               <Card className="h-100">
                 <CardBody className={cardBodyClassZone}>
                   <CardTitle
